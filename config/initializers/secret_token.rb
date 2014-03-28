765738d015ef8c217cb43769c2523e7ec24569a0
Raymond Lim <rclim_ph@me.com>
2014-03-28 12:33:54 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Realtor::Application.config.secret_key_base = 'af55ec8ddaa96e9b4a8714b9fc19c05d0ed204809f93c0a2fab1099ba6e3cbedb2a9d4537687320f524318d43ac6eb0c524a4b4a3b862c3531307262bceb0885'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Realtor::Application.config.session_store :cookie_store, key: '_realtor_session'
